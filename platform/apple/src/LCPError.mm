0b0b5278b2c1159467f6cc4c1ed471f48687f848
Mickaël Menu <mmenu@mantano.com>
2015-12-14 14:06:48 +0100

    Add missing LCP errors in the Obj-C bridge

--- platform/apple/src/LCPError.mm
+++ platform/apple/src/LCPError.mm
@@ -12,8 +12,33 @@ NSString *const LCPErrorExtensionKey = @"LCPErrorExtension";
 
 using namespace lcp;
 
-NSInteger const LCPErrorDecryptionLicenseEncrypted = StatusCode::ErrorDecryptionLicenseEncrypted;
+NSInteger const LCPErrorCommonFail = StatusCode::ErrorCommonFail;
+NSInteger const LCPErrorCommonNoNetProvider = StatusCode::ErrorCommonNoNetProvider;
+NSInteger const LCPErrorCommonNoStorageProvider = StatusCode::ErrorCommonNoStorageProvider;
+NSInteger const LCPErrorCommonEncryptionProfileNotFound = StatusCode::ErrorCommonEncryptionProfileNotFound;
+NSInteger const LCPErrorCommonAlgorithmMismatch = StatusCode::ErrorCommonAlgorithmMismatch;
+NSInteger const LCPErrorOpeningLicenseNotValid = StatusCode::ErrorOpeningLicenseNotValid;
+NSInteger const LCPErrorOpeningLicenseNotStarted = StatusCode::ErrorOpeningLicenseNotStarted;
+NSInteger const LCPErrorOpeningLicenseExpired = StatusCode::ErrorOpeningLicenseExpired;
+NSInteger const LCPErrorOpeningLicenseSignatureNotValid = StatusCode::ErrorOpeningLicenseSignatureNotValid;
+NSInteger const LCPErrorOpeningNoRootCertificate = StatusCode::ErrorOpeningNoRootCertificate;
+NSInteger const LCPErrorOpeningRootCertificateNotValid = StatusCode::ErrorOpeningRootCertificateNotValid;
+NSInteger const LCPErrorOpeningRootCertificateSignatureAlgorithmNotFound = StatusCode::ErrorOpeningRootCertificateSignatureAlgorithmNotFound;
+NSInteger const LCPErrorOpeningContentProviderCertificateNotValid = StatusCode::ErrorOpeningContentProviderCertificateNotValid;
+NSInteger const LCPErrorOpeningContentProviderCertificateNotVerified = StatusCode::ErrorOpeningContentProviderCertificateNotVerified;
+NSInteger const LCPErrorOpeningContentProviderCertificateRevoked = StatusCode::ErrorOpeningContentProviderCertificateRevoked;
+NSInteger const LCPErrorOpeningContentProviderCertificateNotStarted = StatusCode::ErrorOpeningContentProviderCertificateNotStarted;
+NSInteger const LCPErrorOpeningContentProviderCertificateExpired = StatusCode::ErrorOpeningContentProviderCertificateExpired;
+NSInteger const LCPErrorAcquisitionNoAcquisitionLink = StatusCode::ErrorAcquisitionNoAcquisitionLink;
+NSInteger const LCPErrorAcquisitionPublicationCorrupted = StatusCode::ErrorAcquisitionPublicationCorrupted;
+NSInteger const LCPErrorAcquisitionPublicationWrongType = StatusCode::ErrorAcquisitionPublicationWrongType;
+NSInteger const LCPErrorAcquisitionInvalidFilePath = StatusCode::ErrorAcquisitionInvalidFilePath;
 NSInteger const LCPErrorDecryptionUserPassphraseNotValid = StatusCode::ErrorDecryptionUserPassphraseNotValid;
+NSInteger const LCPErrorDecryptionLicenseEncrypted = StatusCode::ErrorDecryptionLicenseEncrypted;
+NSInteger const LCPErrorDecryptionPublicationEncrypted = StatusCode::ErrorDecryptionPublicationEncrypted;
+NSInteger const LCPErrorDecryptionCommonError = StatusCode::ErrorDecryptionCommonError;
+NSInteger const LCPErrorNetworkingRequestNotFound = StatusCode::ErrorNetworkingRequestNotFound;
+NSInteger const LCPErrorNetworkingRequestFailed = StatusCode::ErrorNetworkingRequestFailed;
 
 NSError *LCPErrorFromStatus(Status status)
 {
