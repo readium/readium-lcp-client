73c499d57d3282372c30ee1a0f96795e04cbbe05
danielweck <daniel.weck@gmail.com>
2016-10-19 00:10:16 +0100

    JNI function signature was missing (runtime error...yeah, this is not revealed at compile time)

--- platform/android/lcp/src/main/jni/Service.h
+++ platform/android/lcp/src/main/jni/Service.h
@@ -17,9 +17,11 @@ namespace lcp {
     };
 }
 
- JNIEXPORT jobject JNICALL Java_org_readium_sdk_lcp_Service_nativeOpenLicense(
+JNIEXPORT jobject JNICALL Java_org_readium_sdk_lcp_Service_nativeOpenLicense(
          JNIEnv *env, jobject obj, jlong servicePtr, jstring jLicenseJson);
 
+JNIEXPORT void JNICALL Java_org_readium_sdk_lcp_Service_nativeSetLicenseStatusDocumentProcessingCancelled(
+        JNIEnv *env, jobject obj, jlong servicePtr);
 
 #ifdef __cplusplus
 }
